2416644006fc8023ec00e9c8459d0dd8a22bca3e
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-11-10 11:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 app.listen(port , ()=>{
     console.log('server is up on ' + port);
